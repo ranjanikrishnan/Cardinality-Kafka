d5db38a837b48eddaf53dc625bdb32233ca2aa38
Ranjani Krishnan <ranjanikrishnanr@gmail.com>
2020-06-25 17:22:12 +0200

    minor fixes

--- src/kafka/producer.py
+++ src/kafka/producer.py
@@ -5,13 +5,13 @@ import json
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
-filename = 'temp.jsonl'
+filename = 'stream.jsonl'
 with jsonlines.open(filename) as f:
     for line in f:
         try:
             line = f.read()
-            print('line: ', line)
             data = producer.send('sample', json.dumps(line).encode('utf-8'))
             data.get(timeout=60)
         except EOFError:
             break
+    print('Done.')
